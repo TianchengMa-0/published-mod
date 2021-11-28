30551228b52ce1e7bf77797434e74d5aed02c05c
TianchengMa-0 <59636250+TianchengMa-0@users.noreply.github.com>
2021-11-27 19:04:05 -0800

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+name = "random"
+description = "whatever"
+author = "Felix_Ma"
+version = "1.0"
+forumthread = ""
+
+api_version = 6
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
